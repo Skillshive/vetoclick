865bf3ffb93d8d3214aeca6ec70c35817a62f4da
Bounafaa Abdellah <abdllahbounafaa@gmail.com>
2025-08-21 00:16:57 +0100

    commit breads

--- database/migrations/2025_08_20_230151_create_breads_table.php
+++ database/migrations/2025_08_20_230151_create_breads_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('breads', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 20)->unique();
+            $table->string('description', 100)->nullable();
+            $table->string('avg_weight')->nullable();
             $table->timestamps();
         });
     }
