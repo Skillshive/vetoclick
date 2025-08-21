d58a1bd66db5a0120c8f355b16adffb33e118386
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-21 02:21:29 +0100

    pets table

--- database/migrations/2025_08_20_230309_create_pets_table.php
+++ database/migrations/2025_08_20_230309_create_pets_table.php
@@ -12,9 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+    $table->id();
+    $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+    $table->string('name');
+    $table->string('species');
+    $table->foreignId('breed_id')->nullable()->constrained('breeds');
+    $table->enum('sex', ['Male', 'Female']);
+    $table->boolean('neutered_status')->default(false);
+    $table->date('dob')->nullable();
+    $table->string('microchip_ref')->nullable();
+    $table->string('profile_img')->nullable();
+    $table->decimal('weight_kg', 5, 2)->nullable();
+    $table->tinyInteger('bcs')->nullable(); // Body Condition Score
+    $table->string('color')->nullable();
+    $table->text('notes')->nullable();
+    $table->date('deceased_at')->nullable();
+    $table->timestamps();
+});
     }
 
     /**
