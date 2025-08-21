30b61a8dd78e2ca82608bed0ba6bc9f96acd0f6b
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-21 02:20:43 +0100

    vet table

--- database/migrations/2025_08_20_230119_create_veterinaries_table.php
+++ database/migrations/2025_08_20_230119_create_veterinaries_table.php
@@ -11,10 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('veterinaries', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+       Schema::create('veterinarians', function (Blueprint $table) {
+    $table->id();
+    $table->string('license_number')->unique();
+    $table->string('first_name');
+    $table->string('last_name');
+    $table->string('email')->unique();
+    $table->string('password_hash');
+    $table->string('phone')->nullable();
+    $table->string('specialization')->nullable();
+    $table->integer('years_experience')->nullable();
+    $table->string('profile_img')->nullable();
+    $table->foreignId('subscription_plan_id')->nullable()->constrained('subscription_plans');
+    $table->enum('subscription_status', ['active', 'inactive', 'suspended'])->default('inactive');
+    $table->date('subscription_start_date')->nullable();
+    $table->date('subscription_end_date')->nullable();
+    $table->timestamps();
+});
+
+
     }
 
     /**
