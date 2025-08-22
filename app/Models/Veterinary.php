a74814506a6b3e2fb356bdafa822d2c9b591e27d
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:09:11 +0100

    vet

--- app/Models/Veterinary.php
+++ app/Models/Veterinary.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Veterinary extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'license_number', 'first_name', 'last_name', 'email', 'password', 
+        'phone', 'specialization', 'years_experience', 'profile_img',
+        'subscription_plan_id', 'subscription_status', 'subscription_start_date',
+        'subscription_end_date'
+    ];
+
+    // Relations
+    public function clients()
+    {
+        return $this->belongsToMany(Client::class, 'clients_vets', 'vet_id', 'client_id');
+    }
+
+    public function availabilitySchedules()
+    {
+        return $this->hasMany(Availability::class, 'veterinary_id');
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class, 'veterinary_id');
+    }
+
+    public function vaccinations()
+    {
+        return $this->hasMany(Vaccination::class, 'vet_id');
+    }
+
+  
 }
