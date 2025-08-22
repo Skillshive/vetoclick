cda6dffc054789ee6d51fbce645771396ddae7ab
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:10:20 +0100

    breed

--- app/Models/Breed.php
+++ app/Models/Breed.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Breed extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'species',            // Animal species (Dog, Cat, Bird, etc.)
+        'breed_name',         
+        'avg_weight_kg',      
+        'life_span_years',    
+        'common_health_issues'
+    ];
+
+    // Relationships
+
+    // One breed can have many pets
+    public function pets()
+    {
+        return $this->hasMany(Pet::class);
+    }
+
+    // Optional: one breed can have multiple vaccination schedules
+    
 }
