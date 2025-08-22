aa60045e507bf348b9b294fc7ee6d398019dc1f8
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:08:37 +0100

    supp

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'address', 'city', 'postal_code', 'country', 'is_active'
+    ];
+
+    public function contacts()
+    {
+        return $this->hasMany(SupplierContact::class);
+    }
+
+    public function inventory()
+    {
+        return $this->hasMany(Inventory::class);
+    }
 }
