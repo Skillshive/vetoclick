7208037a6acee3bd06092fbc2a26c0fae18c44e7
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:11:09 +0100

    breeds tab

---
+++ database/migrations/2025_08_21_133102_create_breeds_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {Schema::create('breeds', function (Blueprint $table) {
+    $table->id();
+    $table->string('species');
+    $table->string('breed_name');
+    $table->decimal('avg_weight_kg', 5, 2)->nullable();
+    $table->integer('life_span_years')->nullable();
+    $table->text('common_health_issues')->nullable();
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('breeds');
+    }
+};
