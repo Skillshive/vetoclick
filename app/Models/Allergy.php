5dbe60cf316166053ccd879b17f08bbdfdda0e3c
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 18:06:30 +0100

    allergy model

--- app/Models/Allergy.php
+++ app/Models/Allergy.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Allergy extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'pet_id',
+        'allergy_name',
+        'reaction',
+        'notes',
+    ];
+
+    public function pet()
+    {
+        return $this->belongsTo(Pet::class);
+    }
 }
