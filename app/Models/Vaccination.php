12413830d6fd738802a207f7a2e6cfb159659541
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:09:00 +0100

    vacc

--- app/Models/Vaccination.php
+++ app/Models/Vaccination.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vaccination extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'pet_id', 'vaccin_id', 'conseil_id', 'date_given', 'vet_id',
+        'injection_site', 'next_due_date', 'notes'
+    ];
+
+    public function pet()
+    {
+        return $this->belongsTo(Pet::class);
+    }
+
+    public function veterinarian()
+    {
+        return $this->belongsTo(Veterinary::class, 'vet_id');
+    }
+
+    public function consultation()
+    {
+        return $this->belongsTo(Consultation::class, 'conseil_id');
+    }
+
+    public function vaccine()
+    {
+        return $this->belongsTo(Vaccin::class, 'vaccin_id');
+    }
 }
