9b0c7b97c8d71b2c471bc8a20dd2a051371288a8
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 18:06:48 +0100

    Pres model

--- app/Models/Prescription.php
+++ app/Models/Prescription.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Prescription extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'medical_record_id',
+        'medication_name',
+        'dosage',
+        'frequency',
+        'instructions',
+        'start_date',
+        'end_date',
+    ];
+
+    public function medicalRecord()
+    {
+        return $this->belongsTo(MedicalRecord::class);
+    }
 }
