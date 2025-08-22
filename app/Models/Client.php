22030bfb3d526207963cf552b306f095e765462b
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:07:26 +0100

    cllient model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name', 'last_name', 'email', 'phone', 'mobile',
+        'address', 'city', 'postal_code'
+    ];
+
+    public function veterinarians()
+    {
+        return $this->belongsToMany(Veterinary::class, 'clients_vets', 'client_id', 'vet_id');
+    }
+
+    public function pets()
+    {
+        return $this->hasMany(Pet::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
