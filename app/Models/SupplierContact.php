d25e8f7d62595a2743f3612d3e9c92b54530119f
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-22 15:08:47 +0100

    sup cont

--- app/Models/SupplierContact.php
+++ app/Models/SupplierContact.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SupplierContact extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'supplier_id', 'contact_name', 'phone', 'fixe', 'email', 'position', 'is_primary'
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
 }
