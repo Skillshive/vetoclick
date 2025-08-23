7a9100726ba06a17f67186ae10dcc0ca216ceec9
Bounafaa Abdellah <abdllahbounafaa@gmail.com>
2025-08-23 19:26:26 +0100

    add common dto Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

---
+++ app/common/Dto.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\common;
+
+use App\Interfaces\DtoInterface;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+abstract class DTO implements DtoInterface
+{
+    public static function fromJson($data)
+    {
+        if (is_string($data)) {
+            $data = json_decode($data, true);
+        }
+
+        return static::fromArray($data);
+    }
+
+    public static function fromRequest(Request $request)
+    {
+        return static::fromArray($request->all());
+    }
+
+    public static function fromModel(Model $model)
+    {
+        return static::fromArray($model->toArray());
+    }
+
+    abstract protected static function fromArray(array $data);
+}
