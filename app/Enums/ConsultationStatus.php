0b882fb9ae57dbb5330afbb5b86b1292e390b565
Bounafaa Abdellah <abdllahbounafaa@gmail.com>
2025-08-24 18:07:13 +0100

    add consulattion status enum
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

---
+++ app/Enums/ConsultationStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enums;
+
+enum ConsultationStatus: int
+{
+    case COMPLETED = 1;
+    case IN_PROGRESS = 2;
+    case CANCELLED = 3;
+
+    public function text()
+    {
+        return match ($this) {
+            self::COMPLETED => 'completed',
+            self::IN_PROGRESS => 'in_progress',
+            self::CANCELLED => 'cancelled',
+        };
+    }
+
+    public static function toArray()
+    {
+        return [
+            self::COMPLETED->value => self::COMPLETED->text(),
+            self::IN_PROGRESS->value => self::IN_PROGRESS->text(),
+            self::CANCELLED->value => self::CANCELLED->text(),
+        ];
+    }
+}
