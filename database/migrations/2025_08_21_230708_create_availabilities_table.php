941bfb2a29c95873d43a831c8f13087a9c03e132
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-26 16:00:47 +0100

    upd Schema

--- database/migrations/2025_08_21_230708_create_availabilities_table.php
+++ database/migrations/2025_08_21_230708_create_availabilities_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
     
-        Schema::create('availabilities', function (Blueprint $table) {
+    Schema::create('availabilities', function (Blueprint $table) {
     $table->id();
     $table->uuid('uuid')->unique();
     $table->foreignId('veterinarian_id')->constrained('veterinarians')->onDelete('cascade');
