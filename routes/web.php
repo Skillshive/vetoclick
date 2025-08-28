d7446b508eafd63adc70d98b262f6cf1e2204898
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-28 18:25:30 +0100

    add routes Supplier Product_Cate and Product

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,19 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('species', \App\Http\Controllers\SpeciesController::class);
 });
 
+// Supplier routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('suppliers', \App\Http\Controllers\SupplierController::class);
+});
+
+// Product Category routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('product-categories', \App\Http\Controllers\ProductCategoryController::class);
+});
+
+// Product routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('products', \App\Http\Controllers\ProductController::class);
+});
+
 require __DIR__.'/auth.php';
