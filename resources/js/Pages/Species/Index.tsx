fad3630d403fc88cddf5847c882ff2f5413aa177
Bounafaa Abdellah <abdllahbounafaa@gmail.com>
2025-08-29 11:01:43 +0100

    add layout to species page
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

--- resources/js/Pages/Species/Index.tsx
+++ resources/js/Pages/Species/Index.tsx
@@ -1,6 +1,12 @@
+import MainLayout from '@/layouts/MainLayout';
 import SpeciesDatatable from './datatable';
 import { SpeciesManagementPageProps } from "@/types/Species";
+import { Page } from '@/components/shared/Page';
 
 export default function Index({species, filters}: SpeciesManagementPageProps) {
-    return <SpeciesDatatable species={species} filters={filters} />;
+    return     <MainLayout>
+          <Page title="Species">
+            <SpeciesDatatable species={species} filters={filters} />
+          </Page>
+        </MainLayout>
 }
