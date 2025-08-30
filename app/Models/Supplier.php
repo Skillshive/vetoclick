d3803efe6604abe0e851731591238d3eff427059
manarkjidaa <kjidaamanar09@gmail.com>
2025-08-30 17:29:03 +0100

    Update SupplierModel

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -4,14 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Supplier extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name', 'address', 'city', 'postal_code', 'country', 'is_active'
+        'uuid', 'name', 'email', 'phone', 'address'
     ];
+
+    protected $dates = ['deleted_at'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = Str::uuid();
+            }
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 
     public function contacts()
     {
