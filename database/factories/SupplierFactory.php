eb74c9816f7f372d8face26b95a7e5945f4d23c9
manarkjidaa <kjidaamanar09@gmail.com>
2025-09-02 21:23:53 +0100

    supp factory

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Supplier;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Supplier::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'email' => $this->faker->unique()->companyEmail(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+        ];
+    }
+
+    /**
+     * Indicate that the supplier should not have an email.
+     */
+    public function withoutEmail(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the supplier should not have a phone.
+     */
+    public function withoutPhone(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'phone' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the supplier should not have an address.
+     */
+    public function withoutAddress(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'address' => null,
+        ]);
+    }
+}
