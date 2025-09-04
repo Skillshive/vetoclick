1cd52be2006b241006970a35ca8be360e842349e
manarkjidaa <kjidaamanar09@gmail.com>
2025-09-04 18:05:06 +0100

    Add permission model

---
+++ app/Models/Permission.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUuid;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    use HasUuid;
+
+    protected $fillable = ['name', 'guard_name', 'uuid',"grp_id"];
+    
+    public function group(){
+        return $this->belongsTo(PermissionGroup::class);
+    }
+}
