03981453e79e0a612b656861108c75b378d03f95
manarkjidaa <kjidaamanar09@gmail.com>
2025-09-06 15:55:49 +0100

    update blogDto

--- app/DTOs/BlogDto.php
+++ app/DTOs/BlogDto.php
@@ -5,6 +5,7 @@ namespace App\DTOs;
 use App\common\DTO;
 use App\Interfaces\DTOInterface;
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
 
 class BlogDto extends DTO implements DTOInterface
 {
@@ -14,12 +15,12 @@ class BlogDto extends DTO implements DTOInterface
         public string $title = '',
         public string $body = '',
         public string $caption = '',
-        public ?int $image_id = null,
+        public ?UploadedFile $image_file = null,
         public string $meta_title = '',
         public string $meta_desc = '',
         public string $meta_keywords = '',
         public int $category_blog_id = 0,
-        public array $tags = [],
+        public string $tags = '',
         public ?string $created_at = null,
         public ?string $updated_at = null,
         public ?string $deleted_at = null
@@ -31,12 +32,12 @@ class BlogDto extends DTO implements DTOInterface
             title: $request->input('title', ''),
             body: $request->input('body', ''),
             caption: $request->input('caption', ''),
-            image_id: $request->input('image_id'),
+            image_file: $request->input('image_file'),
             meta_title: $request->input('meta_title', ''),
             meta_desc: $request->input('meta_desc', ''),
             meta_keywords: $request->input('meta_keywords', ''),
             category_blog_id: $request->input('category_blog_id', 0),
-            tags: $request->input('tags', [])
+            tags: $request->input('tags', '')
         );
     }
 
@@ -48,12 +49,12 @@ class BlogDto extends DTO implements DTOInterface
             title: $data['title'] ?? '',
             body: $data['body'] ?? '',
             caption: $data['caption'] ?? '',
-            image_id: $data['image_id'] ?? null,
+            image_file: $data['image_file'] ?? null,
             meta_title: $data['meta_title'] ?? '',
             meta_desc: $data['meta_desc'] ?? '',
             meta_keywords: $data['meta_keywords'] ?? '',
             category_blog_id: $data['category_blog_id'] ?? 0,
-            tags: $data['tags'] ?? [],
+            tags: $data['tags'] ?? '',
             created_at: $data['created_at'] ?? null,
             updated_at: $data['updated_at'] ?? null,
             deleted_at: $data['deleted_at'] ?? null
@@ -66,11 +67,12 @@ class BlogDto extends DTO implements DTOInterface
             'title' => $this->title,
             'body' => $this->body,
             'caption' => $this->caption,
-            'image_id' => $this->image_id,
+            'image_file' => $this->image_file,
             'meta_title' => $this->meta_title,
             'meta_desc' => $this->meta_desc,
             'meta_keywords' => $this->meta_keywords,
             'category_blog_id' => $this->category_blog_id,
+            'tags' => $this->tags,
         ];
     }
 
@@ -82,7 +84,7 @@ class BlogDto extends DTO implements DTOInterface
             'title' => $this->title,
             'body' => $this->body,
             'caption' => $this->caption,
-            'image_id' => $this->image_id,
+            'image_file' => $this->image_file,
             'meta_title' => $this->meta_title,
             'meta_desc' => $this->meta_desc,
             'meta_keywords' => $this->meta_keywords,
@@ -101,7 +103,7 @@ class BlogDto extends DTO implements DTOInterface
             'title' => $this->title,
             'body' => $this->body,
             'caption' => $this->caption,
-            'image_id' => $this->image_id,
+            'image_file' => $this->image_file,
             'meta_title' => $this->meta_title,
             'meta_desc' => $this->meta_desc,
             'meta_keywords' => $this->meta_keywords,
@@ -128,8 +130,8 @@ class BlogDto extends DTO implements DTOInterface
             $data['caption'] = $this->caption;
         }
 
-        if ($this->image_id !== null) {
-            $data['image_id'] = $this->image_id;
+        if ($this->image_file !== null) {
+            $data['image_file'] = $this->image_file;
         }
 
         if (!empty($this->meta_title)) {
@@ -146,6 +148,10 @@ class BlogDto extends DTO implements DTOInterface
 
         if ($this->category_blog_id > 0) {
             $data['category_blog_id'] = $this->category_blog_id;
+        }
+
+        if (!empty($this->tags)) {
+            $data['tags'] = $this->tags;
         }
 
         return $data;
