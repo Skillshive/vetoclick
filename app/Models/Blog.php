2bd5de6a9f4bd72c97f5726d63a19a01b251d934
manarkjidaa <kjidaamanar09@gmail.com>
2025-09-06 15:57:57 +0100

    add hasImage to Blog Model

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\HasImage;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,7 @@ use Illuminate\Support\Str;
 
 class Blog extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes,HasImage;
 
     protected static function boot()
     {
@@ -28,12 +29,10 @@ class Blog extends Model
         'meta_desc',
         'meta_keywords',
         'category_blog_id',
+        'tags',
     ];
 
-    public function image()
-    {
-        return $this->belongsTo(Image::class);
-    }
+
 
     public function categoryBlog()
     {
