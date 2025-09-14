7f26309c67dd7b2b9896c41d254396025faab1c5
manarkjidaa <kjidaamanar09@gmail.com>
2025-09-14 14:49:09 +0100

    update app icons

--- resources/js/menus/items/settings.ts
+++ resources/js/menus/items/settings.ts
@@ -1,20 +1,25 @@
 import { MenuItem } from '../index';
-import { BoxIcon, PawPrint,  PenLine } from "lucide-react";
+import { DogIcon, PawPrint,  PawPrintIcon,  PenLine } from "lucide-react";
 import { TruckIcon, UserIcon, ShieldCheckIcon, CreditCardIcon } from "@heroicons/react/24/outline";
-import { HomeIcon } from "@heroicons/react/24/outline";
+import DashboardsIcon from "@/assets/dualicons/dashboards.svg?react";
+import PetIcon from "@/assets/dualicons/pet.svg?react";
+import BoxIcon from "@/assets/dualicons/box.svg?react";
+import BoxAddIcon from "@/assets/nav-icons/box-add.svg?react";
+import SettingIcon from "@/assets/dualicons/setting.svg?react";
+import LampIcon from "@/assets/dualicons/lamp.svg?react";
 
 const AnimalsMenu: MenuItem[] = [
   {
     id: 'dashboard',
     title: 'common.dashboard',
-    icon: HomeIcon,
+    icon: DashboardsIcon,
     path: route('dashboard'),
     type: 'item' as const,
   },
   {
     id: 'animals',
     title: 'common.menu.animals_management',
-    icon: PawPrint,
+    icon: PetIcon,
     type: 'group' as const,
     submenu: [
       {
@@ -28,7 +33,7 @@ const AnimalsMenu: MenuItem[] = [
   {
     id: 'suppliers',
     title: 'common.menu.suppliers_management',
-    icon: TruckIcon,
+    icon: BoxIcon,
     type: 'group' as const,
     submenu: [
       {
@@ -37,14 +42,6 @@ const AnimalsMenu: MenuItem[] = [
         path: route('suppliers.index'),
         type: 'item' as const,
       },
-    ],
-  },
-  {
-    id: 'Products',
-    title: 'common.menu.products_management',
-    icon: BoxIcon,
-    type: 'group' as const,
-    submenu: [
       {
         id: 'allProducts',
         title: 'common.menu.products_list',
@@ -59,10 +56,11 @@ const AnimalsMenu: MenuItem[] = [
       },
     ],
   },
+
   {
     id: 'Blogs',
     title: 'common.menu.blogs_management',
-    icon: PenLine,
+    icon: LampIcon,
     type: 'group' as const,
     submenu: [
       {
@@ -80,9 +78,9 @@ const AnimalsMenu: MenuItem[] = [
     ],
   },
   {
-    id: 'Users',
-    title: 'common.menu.access_management',
-    icon: UserIcon,
+    id: 'settings',
+    title: 'common.menu.settings',
+    icon: SettingIcon,
     type: 'group' as const,
     submenu: [
       {
@@ -96,27 +94,12 @@ const AnimalsMenu: MenuItem[] = [
         title: 'common.menu.roles_permissions',
         path: route('roles.index'),
         type: 'item' as const,
-      }
-    ],
-  },
-  {
-    id: 'SubscriptionPlans',
-    title: 'common.menu.subscription_plans',
-    icon: CreditCardIcon,
-    type: 'group' as const,
-    submenu: [
-      {
+      },  {
         id: 'allSubscriptionPlans',
         title: 'common.menu.subscription_plans_list',
         path: route('subscription-plans.index'),
         type: 'item' as const,
       },
-      {
-        id: 'featureGroups',
-        title: 'common.feature_groups',
-        path: route('feature-groups.index'),
-        type: 'item' as const,
-      }
     ],
   },
 ];
