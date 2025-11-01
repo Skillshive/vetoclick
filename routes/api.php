fbc8f58c2c16681768300786af293847fb8785b9
Bounafaa Abdellah <abdllahbounafaa@gmail.com>
2025-11-01 02:38:03 +0100

    add appointments api routes
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,16 @@ use App\Http\Controllers\EmailController;
 use App\Http\Controllers\SubscriptionPlanController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\AppointmentController;
 
-// Email Service API Routes
 Route::prefix('email')->controller(EmailController::class)->group(function () {
     Route::post('/send', 'sendEmail');
     Route::post('/send-template', 'sendTemplateEmail');
     Route::post('/send-bulk', 'sendBulkEmails');
 });
 
-// Subscription Plans API Routes
 Route::get('/subscription-plans/active', [SubscriptionPlanController::class, 'activePlans']);
 
-// Product API Routes
 Route::prefix('products')->controller(ProductController::class)->group(function () {
     Route::get('/', 'apiIndex');
     Route::get('/search', 'search');
@@ -28,10 +26,11 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
     Route::get('/export/csv', 'export');
 });
 
-// Blog API Routes
 Route::prefix('blogs')->controller(BlogController::class)->group(function () {
     Route::get('/', 'apiIndex');
 });
 
-
-// Address validation is now handled by web routes
+Route::prefix('appointments')->controller(AppointmentController::class)->group(function () {
+    Route::post('/', 'store');
+    Route::post('/{uuid}/update', 'update');
+});
