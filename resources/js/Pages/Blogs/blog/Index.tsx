5ebdaaecad850eb66410f145a94ee9b987839b9a
manarkjidaa <kjidaamanar09@gmail.com>
2025-11-09 16:48:45 +0100

    increase blog card grid layout to four columns on larger screens

--- resources/js/Pages/Blogs/blog/Index.tsx
+++ resources/js/Pages/Blogs/blog/Index.tsx
@@ -112,7 +112,7 @@ export default function Index({
         </Card>
       ) : (
         <>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
             {blogsList.map((blog) => (
               <BlogCard
                 key={blog.uuid}
