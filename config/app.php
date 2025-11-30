08578c3a651676643b60b17abe0a4c3d39bda838
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2025-11-30 22:54:32 +0100

    Add Twilio configuration to app settings
    - Introduced Twilio settings including account SID, auth token, phone number, and messaging service SID to facilitate SMS functionalities.
    
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

--- config/app.php
+++ config/app.php
@@ -122,5 +122,10 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+'twilio' => [
+    'account_sid' => env('TWILIO_ACCOUNT_SID'),
+    'auth_token' => env('TWILIO_AUTH_TOKEN'),
+    'phone_number' => env('TWILIO_PHONE_NUMBER'),
+    'messaging_service_sid' => env('TWILIO_MESSAGING_SERVICE_SID'),
+],
 ];
