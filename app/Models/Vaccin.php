0fb463f3579988ddadd8d69a3e19f5bb5d9ea424
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2025-12-05 01:39:27 +0100

    Add UUID generation and relationships in Vaccin model
    - Implemented UUID generation during the creation of Vaccin instances to ensure unique identification.
    - Defined fillable attributes for the Vaccin model, enhancing data handling.
    - Established a one-to-many relationship with the Vaccination model, allowing for better management of associated vaccinations.
    
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

--- app/Models/Vaccin.php
+++ app/Models/Vaccin.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 
 class Vaccin extends Model
 {
-    //
+    use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($vaccin) {
+            if (!$vaccin->uuid) {
+                $vaccin->uuid = Str::uuid();
+            }
+        });
+    }
+
+    protected $fillable = [
+        'vaccin_name',
+        'description',
+        'species_type',
+    ];
+
+    public function vaccinations()
+    {
+        return $this->hasMany(Vaccination::class, 'vaccine_id');
+    }
 }
