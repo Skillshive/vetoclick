e4ff9325ad713d584293075640d6c650f174222e
Abdellah-Bounafaa <abdllahbounafaa@gmail.com>
2026-01-31 13:34:44 +0100

    Update Grid Layout in Pets Index Component for Improved Responsiveness
    - Adjusted the grid layout in the Pets Index component to enhance responsiveness across different screen sizes.
    - Changed the number of columns for larger screens to ensure a better visual arrangement of pet cards.
    
    These changes improve the overall user experience by providing a more adaptable and visually appealing layout for pet listings.
    
    Co-authored-by: kjidaamanar <kjidaamanar09@gmail.com>

--- resources/js/Pages/Pets/Index.tsx
+++ resources/js/Pages/Pets/Index.tsx
@@ -119,7 +119,7 @@ export default function Index({
             </Card>
           ) : (
             <>
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3">
                 {petsList.map((pet) => (
                   <PetCard
                     key={pet.uuid}
